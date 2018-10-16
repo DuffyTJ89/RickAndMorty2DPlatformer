fb38daeb2a4ad33d6a8902b63247d80422ec04da
DuffyTJ89 <tomduffy89@gmail.com>
2018-10-16 18:25:31 +0100

    Player dies and restarts game;

--- Rick&Morty/Assets/Rick_Health.cs
+++ Rick&Morty/Assets/Rick_Health.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Rick_Health : MonoBehaviour {
+public class Rick_Health : MonoBehaviour
+{
 
     public int health;
     public bool hasDied;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Use this for initialization
+    void Start()
+    {
+        hasDied = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.transform.position.y < -7)
+        {
+            //Debug.Log("Player has died"); //check to see if the player triggers this below the postion
+            hasDied = true;
+        }
+        if (hasDied == true)
+        {
+            StartCoroutine("Die"); //StartCoroutine function always returns immediately
+        }
+    }
+        IEnumerator Die()
+        {
+            SceneManager.LoadScene("FirstScene"); //start a new game if the player has died
+            yield return null;
+        }
+
+   
 }
