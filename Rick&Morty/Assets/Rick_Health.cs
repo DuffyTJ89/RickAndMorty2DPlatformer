f8732e8c28fce978d0e5bd095ceff3fb109cb5d0
DuffyTJ89 <tomduffy89@gmail.com>
2018-10-16 17:29:04 +0100

    added camera movement;

---
+++ Rick&Morty/Assets/Rick_Health.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rick_Health : MonoBehaviour {
+
+    public int health;
+    public bool hasDied;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
