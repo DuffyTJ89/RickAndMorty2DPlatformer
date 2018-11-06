42291c4ee017da0b9e8b6c2996468f7189936f8b
DuffyTJ89 <tomduffy89@gmail.com>
2018-11-06 16:20:15 +0000

    main menu working

---
+++ Rick&Morty/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	public void Play () {
+        SceneManager.LoadScene(1);// start a new game by calling the first scene
+    }
+	
+	// Update is called once per frame
+	public void Quit () {
+        Application.Quit();
+	}
+}
